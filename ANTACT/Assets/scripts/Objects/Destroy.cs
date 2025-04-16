84c3ee94c5d5aa5dde345f8678ef5a8f68e49412
Taegun <ptg0418@gmail.com>
2025-04-16 22:45:49 +0900

    Prototype_Map_v.1

--- ANTACT/Assets/scripts/Objects/Destroy.cs
+++ ANTACT/Assets/scripts/Objects/Destroy.cs
@@ -15,7 +15,7 @@ public class Destroy : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D collision)
 {
-    Destroy(collision.gameObject);
+    Destroy(gameObject);
     Debug.Log("Destroy");
 }
 }
